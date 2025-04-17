cab7e79bf68daa0778d7093bef7b5524865f083f
lsimovicc <lazar.simovic@logeecom.com>
2025-04-17 15:29:57 +0200

    Created model Category

---
+++ DemoProject/model/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoProject.model;
+
+public class Category
+{
+    private Guid id { get; set; } = new Guid();
+    private String title { get; set; }
+    private String description { get; set; }
+    private Category category { get; set; }
+    
+}
