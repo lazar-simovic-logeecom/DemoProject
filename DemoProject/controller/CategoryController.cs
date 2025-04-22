e28aa74903d9ef31b7b9a25b99dbe2c534a842e5
lsimovicc <lazar.simovic@logeecom.com>
2025-04-22 09:21:30 +0200

    Adding Get and Post methods

---
+++ DemoProject/controller/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using DemoProject.model;
+using DemoProject.service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProject.controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoryController : ControllerBase
+{
+
+    public readonly CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService)
+    {
+        this.categoryService = categoryService;
+    }
+    
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        categoryService.addCategory(category);
+        return Ok(category);
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(categoryService.getAllCategory());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        return Ok(categoryService.getCategoryById(id));
+    }
+
+    /*[HttpPut("{id}")]
+    public IActionResult Update(Guid id, Category category)
+    {
+        
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        
+    }*/
+    
+}
