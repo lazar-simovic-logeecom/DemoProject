e192add84c5cbb113bde6eefbc5cf64d4bba1113
lsimovicc <lazar.simovic@logeecom.com>
2025-04-22 09:20:57 +0200

    Creating service layer

---
+++ DemoProject/service/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using DemoProject.model;
+
+namespace DemoProject.service;
+
+public class CategoryService
+{
+
+    public static List<Category> listCategory = new List<Category>();
+
+    public List<Category> getAllCategory()
+    {
+        return listCategory;
+    }
+
+    public Category getCategoryById(Guid id)
+    {
+        return listCategory.FirstOrDefault(x => x.Id == id);
+    }
+
+    public void addCategory(Category category)
+    {
+        category.Id = Guid.NewGuid();
+        listCategory.Add(category);
+    }
+
+    public void updateCategory(Category category)
+    {
+        listCategory.Remove(category);
+    }
+
+    public void deleteCategory(Category category)
+    {
+        listCategory.Remove(category);
+    }
+    
+}
