2990e2adc4376e8bdc6e6d2591e3f15b5e00cdf4
lsimovicc <lazar.simovic@logeecom.com>
2025-04-24 09:49:46 +0200

    IONJ-124 Adding dto

---
+++ DemoProject/Dto/CategoryDto.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoProject.Dto;
+using System.ComponentModel.DataAnnotations;
+public class CategoryDto
+{ 
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title can't be longer than 100 characters.")]
+        public string Title { get; set; }
+
+        [StringLength(100, ErrorMessage = "Description can't be longer than 100 characters.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Code is required.")]
+        [StringLength(100, ErrorMessage = "Code can't be longer than 100 characters.")]
+        public string Code { get; set; }
+
+        public Guid? ParentCategory { get; set; }
+}
