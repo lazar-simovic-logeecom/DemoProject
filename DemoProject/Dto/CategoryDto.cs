3c3c9df7e840f820d548adb36359424404cb1f64
lsimovicc <lazar.simovic@logeecom.com>
2025-04-29 14:09:58 +0200

    IONJ-134 Refactoring dto

--- DemoProject/Dto/CategoryDto.cs
+++ DemoProject/Dto/CategoryDto.cs
@@ -3,9 +3,9 @@ namespace DemoProject.Dto;
 
 public class CategoryDto
 { 
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Code { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required string Code { get; set; }
         
         public Guid? ParentCategory { get; set; }
 }
