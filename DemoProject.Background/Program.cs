6b522e9e3076b0f62e1a831b9c602349f798184a
lsimovicc <lazar.simovic@logeecom.com>
2025-05-15 15:36:03 +0200

    IONJ-159 Implemented appsettings.json

--- DemoProject.Background/Program.cs
+++ DemoProject.Background/Program.cs
@@ -4,23 +4,24 @@ using DemoProject.Application.Services;
 using DemoProject.Background;
 using DemoProject.DataEF;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) =>
+    .ConfigureServices((hostContext, services) =>
     {
+        var configuration = hostContext.Configuration;
+
         services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql("Host=localhost;Port=5432;Database=DemoProjectDb;Username=user;Password=pass"));
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddSingleton(Channel.CreateUnbounded<string>());
+        services.AddSingleton<LoggingService>();
+        services.AddHostedService<LoggingBackgroundService>();
+        services.AddHostedService<DeleteBackgroundService>();
+        services.AddScoped<ICategoryRepository, CategoryRepositoryEf>();
     });
-builder.ConfigureServices((hostContext, services) =>
-{
-    services.AddSingleton(Channel.CreateUnbounded<string>());
-    services.AddSingleton<LoggingService>();
-    services.AddHostedService<LoggingBackgroundService>();
-    services.AddHostedService<DeleteBackgroundService>();
-    services.AddScoped<ICategoryRepository, CategoryRepositoryEf>();
-});
 
 var app = builder.Build();
 
