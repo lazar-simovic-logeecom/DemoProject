8ac22cd26160bd07518a774d7d1ea29ba4c3bc8d
lsimovicc <lazar.simovic@logeecom.com>
2025-05-19 15:18:56 +0200

    IONJ-159 Added appsettings.background.json

--- DemoProject/Program.cs
+++ DemoProject/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Threading.Channels;
 using DemoProject.Mappings;
 using DemoProject.Application;
@@ -7,8 +8,11 @@ using DemoProject.Background;
 using FluentValidation.AspNetCore;
 using DemoProject.Validators;
 using DemoProject.DataEF;
+using DemoProject.Middlewares;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,10 +33,39 @@ builder.Services.AddValidatorsFromAssemblyContaining<CreateCategoryValidator>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
-builder.Services.AddTransient<ICategoryRepository, CategoryRepositoryEf>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepositoryEf>();
+
+builder.Services.AddTransient<IAuthService, AuthService>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!);
+
+var tokenValidationParameters = new TokenValidationParameters
+{
+    ValidateIssuer = false,
+    ValidateAudience = false,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(key)
+};
+
+builder.Services.AddSingleton(tokenValidationParameters);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = tokenValidationParameters;
+    });
+
 
 var app = builder.Build();
+
+//app.UseMiddleware<BasicAuthMiddleware>();
+app.UseMiddleware<JwtAuthMiddleware>();
 
 app.UseHttpsRedirection();
 app.MapControllers();
